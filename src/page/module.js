1daafe9a6ba5f6711d7f39592d59c29b37bf974c
iSunXiaojun <sun_xiaojun_@outlook.com>
2018-01-08 21:46:16 +0800

    initial

---
+++ src/page/module.js
@@ -0,0 +1,11 @@
+/*
+* @Author: iSunXiaojun
+* @Date:   2017-12-30 23:38:00
+* @Last Modified by:   iSunXiaojun
+* @Last Modified time: 2017-12-30 23:38:48
+*/
+
+'use strict';
+module.exports = {
+    test: 1234
+}
